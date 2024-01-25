abb20da3e64048af4326895659d8e47405956048
ancuvi <angeloschmidt18@gmail.com>
2024-01-25 19:45:02 +0100

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,118 +1,21 @@
 import "./../scss/style.scss";
 
-var bg = document.querySelector(".item-bg");
-var items = document.querySelectorAll(".news__item");
-var item = document.querySelector(".news__item");
-
-function cLog(content) {
-  console.log(content);
-}
-
-if ($(window).width() > 800) {
-  $(document).on("mouseover", ".news__item", function (_event, _element) {
-    var newsItem = document.querySelectorAll(".news__item");
-    newsItem.forEach(function (element, index) {
-      element.addEventListener("mouseover", function () {
-        var x = this.getBoundingClientRect().left;
-        var y = this.getBoundingClientRect().top;
-        var width = this.getBoundingClientRect().width;
-        var height = this.getBoundingClientRect().height;
-
-        $(".item-bg").addClass("active");
-        $(".news__item").removeClass("active");
-        // $('.news__item').removeClass('active');
-
-        bg.style.width = width + "px";
-        bg.style.height = height + "px";
-        bg.style.transform = "translateX(" + x + "px ) translateY(" + y + "px)";
-      });
-
-      element.addEventListener("mouseleave", function () {
-        $(".item-bg").removeClass("active");
-        $(".news__item").removeClass("active");
-      });
-    });
-  });
-}
-
-var swiper = new Swiper(".news-slider", {
-  effect: "coverflow",
-  grabCursor: true,
-  loop: true,
-  centeredSlides: true,
-  keyboard: true,
-  spaceBetween: 0,
-  slidesPerView: "auto",
-  speed: 300,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 0,
-    modifier: 3,
-    slideShadows: false,
-  },
-  breakpoints: {
-    480: {
-      spaceBetween: 0,
-      centeredSlides: true,
-    },
-  },
-  simulateTouch: true,
-  navigation: {
-    nextEl: ".news-slider-next",
-    prevEl: ".news-slider-prev",
-  },
-  pagination: {
-    el: ".news-slider__pagination",
-    clickable: true,
-  },
-  on: {
-    init: function () {
-      var activeItem = document.querySelector(".swiper-slide-active");
-
-      var sliderItem = activeItem.querySelector(".news__item");
-
-      $(".swiper-slide-active .news__item").addClass("active");
-
-      var x = sliderItem.getBoundingClientRect().left;
-      var y = sliderItem.getBoundingClientRect().top;
-      var width = sliderItem.getBoundingClientRect().width;
-      var height = sliderItem.getBoundingClientRect().height;
-
-      $(".item-bg").addClass("active");
-
-      bg.style.width = width + "px";
-      bg.style.height = height + "px";
-      bg.style.transform = "translateX(" + x + "px ) translateY(" + y + "px)";
-    },
-  },
-});
-
-swiper.on("touchEnd", function () {
-  $(".news__item").removeClass("active");
-  $(".swiper-slide-active .news__item").addClass("active");
-});
-
-swiper.on("slideChange", function () {
-  $(".news__item").removeClass("active");
-});
-
-swiper.on("slideChangeTransitionEnd", function () {
-  $(".news__item").removeClass("active");
-  var activeItem = document.querySelector(".swiper-slide-active");
-
-  var sliderItem = activeItem.querySelector(".news__item");
-
-  $(".swiper-slide-active .news__item").addClass("active");
-
-  var x = sliderItem.getBoundingClientRect().left;
-  var y = sliderItem.getBoundingClientRect().top;
-  var width = sliderItem.getBoundingClientRect().width;
-  var height = sliderItem.getBoundingClientRect().height;
-
-  $(".item-bg").addClass("active");
-
-  bg.style.width = width + "px";
-  bg.style.height = height + "px";
-  bg.style.transform = "translateX(" + x + "px ) translateY(" + y + "px)";
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  const navbarPlaceholder = document.querySelector(".navbar-placeholder");
+  if (window.scrollY > 100) {
+    navbar.classList.add("navbar-fixed");
+    navbarPlaceholder.style.display = "block";
+    navbarPlaceholder.style.height = navbar.offsetHeight + "px";
+    // Hinzufügen der navbar-visible Klasse ohne Verzögerung
+    navbar.classList.add("navbar-visible");
+  } else {
+    // Entfernen der navbar-visible Klasse und warten auf das Ende der Opacity-Animation
+    navbar.classList.remove("navbar-visible");
+    setTimeout(() => {
+      navbar.classList.remove("navbar-fixed");
+      navbarPlaceholder.style.display = "none";
+      navbarPlaceholder.style.height = "0";
+    }, 300); // Diese Verzögerung muss der Übergangszeit entsprechen
+  }
 });
