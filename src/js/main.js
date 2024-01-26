5a8cc102a072054140c685da007fcfd5b1e2fb5a
ancuvi <angeloschmidt18@gmail.com>
2024-01-26 13:21:26 +0100

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
 import "./../scss/style.scss";
+
+// global scope (Gültigkeitsbereich)
+var x = "Hello, Geat day";
+
+function example() {
+  console.log(x);
+}
+example();
+console.log(x); //geht weil var x global definiert ist
+
+//function scope
+function example2() {
+  var fs = "Hallo greater";
+  console.log(fs);
+}
+example2();
+//console.log(fs); // geht nicht weil fs nur innerhalb der funktion definiert ist
+
+// Block Scope
+function example3() {
+  if (true) {
+    let bv = "hallo welt";
+    console.log(bv);
+  }
+  //console.log(bv); geht nicht weil bv nur innerhalb des Blocks definiert ist
+}
+example3();
+//console.log(bv); geht dementsprechend auch nicht.
