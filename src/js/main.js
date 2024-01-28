96b691e693fca2c42849d2838a98fafbc72174ec
ancuvi <angeloschmidt18@gmail.com>
2024-01-28 10:15:37 +0100

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,1 @@
 import "./../scss/style.scss";
-
-// Lotto-Zahlen Generator
-function generateLottoNumbers() {
-  let lottoNumbers = [];
-
-  while (lottoNumbers.length < 6) {
-    let randomNumber = Math.floor(Math.random() * 49) + 1;
-
-    if (lottoNumbers.indexOf(randomNumber) === -1) {
-      lottoNumbers.push(randomNumber);
-    }
-  }
-
-  return lottoNumbers;
-}
-
-console.log(generateLottoNumbers());
